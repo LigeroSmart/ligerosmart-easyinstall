f3d5003ed69a9410cebfc5e827a6e10ab4a35510
Marcelo Matos <marcelofmatos@gmail.com>
2020-04-05 20:31:34 -0300

    init file to run LigeroSmart on windows system

---
+++ for-windows/ligerosmart-easyinstall.sh
@@ -0,0 +1,23 @@
+# LigeroSmart Easy Install for windows
+#
+# Sorry... This is not implemented yet :/
+#
+# But you can visit this sites references about docker on windows:
+# * https://docs.microsoft.com/pt-br/virtualization/windowscontainers/
+# * https://docs.microsoft.com/en-us/virtualization/windowscontainers/quick-start/set-up-environment?tabs=Windows-Server
+# * https://github.com/MicrosoftDocs/Virtualization-Documentation
+#
+# When you install the Docker Toolbox on Windows, you already have Docker and you also get Docker Compose.
+# * https://docs.docker.com/toolbox/toolbox_install_windows/
+# * https://www.docker.com/blog/docker-toolbox-compose/ 
+#
+# Make sure you have docker-compose installed on your machine by opening up a cmd prompt 
+# or PowerShell console, and run the following command:
+# 
+# docker-compose -v
+#
+# Download this repository https://github.com/LigeroSmart/ligerosmart-stack
+# and run the docker-compose command in there:
+#
+#  
+# Can you help us? Check https://github.com/LigeroSmart/ligerosmart-easyinstall
