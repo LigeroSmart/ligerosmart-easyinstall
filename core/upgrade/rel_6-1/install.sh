eddc96283a63c196ae796320d03cf41367627b3e
Marcelo Matos <marcelofmatos@gmail.com>
2024-03-18 13:01:17 -0300

    feat: upgrade

---
+++ core/upgrade/rel_6-1/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bah
+
+git pull --strategy-option=theirs --allow-unrelated-histories --no-edit rel_6-1
--- ligerosmart/daemon-clean/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# https://github.com/LigeroSmart/ligerosmart/commit/52035e64feed4dc01a6ab57db2978a17c862ace2.patch
-
-curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart/rel-6_1/var/cron/daemon-clean > $HOME/var/cron/daemon-clean
-curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart/rel-6_1/Kernel/System/Console/Command/Maint/Daemon/Clean.pm > $HOME/Kernel/System/Console/Command/Maint/Daemon/Clean.pm
-
-git add $HOME/var/cron/daemon-clean $HOME/Kernel/System/Console/Command/Maint/Daemon/Clean.pm
-git commit -m "fix: daemon clean"
