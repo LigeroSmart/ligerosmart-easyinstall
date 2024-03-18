faa898bedde2d4387a3af9469702663caf04ef38
Marcelo Matos <marcelofmatos@gmail.com>
2024-03-18 13:05:37 -0300

    fix: command

--- core/daemon-clean/install.sh
+++ core/daemon-clean/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+#
+# curl https://get.ligerosmart.com/core/daemon-clean/ | bash
+# 
+# Patch from github
 # https://github.com/LigeroSmart/ligerosmart/commit/52035e64feed4dc01a6ab57db2978a17c862ace2.patch
 
 curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart/rel-6_1/var/cron/daemon-clean > $HOME/var/cron/daemon-clean
--- core/upgrade/rel-6_1/install.sh
+++ core/upgrade/rel-6_1/install.sh
@@ -1,3 +1,5 @@
-#!/bin/bah
-
+#!/bin/bash
+#
+# curl https://get.ligerosmart.com/core/upgrade/rel-6_1/ | bash
+# 
 git pull --strategy-option=theirs --allow-unrelated-histories --no-edit rel_6-1
