4c6d348f80427c3f7c67cf6f762b06cf1c64a95b
Marcelo Matos <marcelofmatos@gmail.com>
2024-03-18 12:54:46 -0300

    fix: git

--- ligerosmart/daemon-clean/install.sh
+++ ligerosmart/daemon-clean/install.sh
@@ -5,3 +5,5 @@
 curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart/rel-6_1/var/cron/daemon-clean > $HOME/var/cron/daemon-clean
 curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart/rel-6_1/Kernel/System/Console/Command/Maint/Daemon/Clean.pm > $HOME/Kernel/System/Console/Command/Maint/Daemon/Clean.pm
 
+git add $HOME/var/cron/daemon-clean $HOME/Kernel/System/Console/Command/Maint/Daemon/Clean.pm
+git commit -m "fix: daemon clean"
