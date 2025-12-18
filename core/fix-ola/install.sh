b0fc339ba33323f53ed8a67d30cb7737ac38db1f
Marcelo Matos <marcelofmatos@gmail.com>
2025-12-18 08:39:37 -0400

    fix: OLA Suspend

---
+++ core/fix-ola/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# curl https://get.ligerosmart.com/core/fix-ola/ | bash
+# 
+
+set -e
+
+cd /opt/otrs
+
+curl https://raw.githubusercontent.com/LigeroSmart/ligerosmart-easyinstall/main/core/fix-ola/0001-fix-OLA-Suspend.patch > /tmp/0001-fix-OLA-Suspend.patch
+
+git apply /tmp/0001-fix-OLA-Suspend.patch
+
+git add Kernel/System/GenericAgent/SetSolutionTimeField.pm
+git add Kernel/System/Ticket/TicketExtensionsStopEscalation.pm
+git add var/httpd/htdocs/js/Complemento.TicketEscalation.js
+
+git commit -m "fix: OLA Suspend"
